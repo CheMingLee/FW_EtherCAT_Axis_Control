5718cecc748017d79966a294a94888bc6f2b3a78
CheMingLee <jmli1993@gmail.com>
2021-02-05 18:32:21 +0800

    0205 update cmd setting

--- src/cmd_function.c
+++ src/cmd_function.c
@@ -49,16 +49,47 @@ void GetAppCmd()
 	if (CheckFlag())
 	{
 		u16 uCmd;
+		u16 usSize;
+		char pData[256];
+
 		uCmd = Xil_In16(IO_ADDR_BRAM_IN_CMD);
+		usSize = Xil_In16(IO_ADDR_BRAM_IN_CMD_SIZE);
+		memcpy(pData, (void *)IO_ADDR_BRAM_IN_DATA, usSize);
 
 		switch (uCmd)
 		{
-			case 1:
+			case CMD_SET_DATASIZE:
 			{
+				memcpy((void *)ECM_ADDR_DATA_BYTE, pData, usSize);
+				break;
+			}
+			case CMD_SET_TXDATA:
+			{
+				memcpy((void *)ECM_ADDR_DATA_OUT, pData, usSize);
+				break;
+			}
+			case CMD_SET_SEND:
+			{
+				Xil_Out32(ECM_ADDR_SEND, 1);
+				break;
+			}
+			case CMD_GET_BUSY:
+			{
+				u32 uBusyBuf;
+				
+				uBusyBuf = Xil_In32(ECM_ADDR_BUSY);
+				memcpy(pData, &uBusyBuf, sizeof(uBusyBuf));
+				CmdGetToApp(uCmd, pData, usSize);
 				SetFlagOutOne();
 				break;
 			}
-			
+			case CMD_GET_RXDATA:
+			{
+				memcpy(pData, (void *)ECM_ADDR_DATA_IN, usSize);
+				CmdGetToApp(uCmd, pData, usSize);
+				SetFlagOutOne();
+				break;
+			}
 			default:
 			{
 				break;
@@ -66,6 +97,16 @@ void GetAppCmd()
 		}
 		SetFlagInZero();
 	}
+}
+
+/*************************************************************************/
+
+void CmdGetToApp(u16 usAck, char *pData, u16 usSize)
+{
+	Xil_Out32(IO_ADDR_BRAM_OUT_SIZE, usSize+4);
+	Xil_Out16(IO_ADDR_BRAM_OUT_ACK, usAck);
+	Xil_Out16(IO_ADDR_BRAM_OUT_ACK_SIZE, usSize);
+	memcpy((void *)IO_ADDR_BRAM_OUT_DATA, pData, usSize);
 }
 
 /*************************************************************************/
--- src/setup_interrupt.c
+++ src/setup_interrupt.c
@@ -2,6 +2,13 @@
 
 XScuGic Intc; //GIC
 
+void ECM_intr_Handler(void *CallBackRef)
+{
+	// to do
+
+	Xil_Out32(ECM_INTR_RESET, 1);
+}
+
 void SetupInterruptSystem()
 {
 	XScuGic *GicInstancePtr = &Intc;
@@ -22,11 +29,4 @@ void SetupInterruptSystem()
 
 	// Enable interrupts in the Processor.
 	Xil_ExceptionEnableMask(XIL_EXCEPTION_IRQ);
-}
-
-void ECM_intr_Handler(void *CallBackRef)
-{
-	// to do
-
-	Xil_Out32(ECM_INTR_RESET, 1);
 }
