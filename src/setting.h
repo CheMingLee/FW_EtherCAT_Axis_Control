936d38893c46f36f94aa60e8ee29d24db47389fd
CheMingLee <jmli1993@gmail.com>
2021-02-22 18:36:20 +0800

    0222 update

--- src/setting.h
+++ src/setting.h
@@ -40,6 +40,11 @@
 #define CMD_SET_SEND 2
 #define CMD_GET_BUSY 3
 #define CMD_GET_RXDATA 4
+#define CMD_SET_PARAMS 5
+#define CMD_SET_JOG 6
+#define CMD_SET_MOTION 7
+#define CMD_SET_HOME 8
+#define CMD_SET_STOP 9
 
 // ECM SPI structure
 ECM_PACK_BEGIN
